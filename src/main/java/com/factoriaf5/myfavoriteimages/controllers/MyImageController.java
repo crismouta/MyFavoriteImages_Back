3c4b952459051b5c34f776af010faaf5829f08ea
crismouta <crismouta@gmail.com>
2023-11-02 22:18:39 +0100

    adding getById, create, update and delete functionalities

--- src/main/java/com/factoriaf5/myfavoriteimages/controllers/MyImageController.java
+++ src/main/java/com/factoriaf5/myfavoriteimages/controllers/MyImageController.java
@@ -3,9 +3,7 @@ package com.factoriaf5.myfavoriteimages.controllers;
 import com.factoriaf5.myfavoriteimages.domain.models.MyImage;
 import com.factoriaf5.myfavoriteimages.domain.services.MyImageService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,4 +21,28 @@ public class MyImageController {
     public ResponseEntity<List<MyImage>> getAll(){
         return ResponseEntity.ok(this.myImageService.findAll());
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<MyImage> getById(@PathVariable Long id) {
+        return ResponseEntity.ok(this.myImageService.findById(id));
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<MyImage> create(@RequestBody MyImage image){
+        return ResponseEntity.ok(this.myImageService.create(image));
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteById(@PathVariable Long id){
+        this.myImageService.delete(id);
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<MyImage> update(@PathVariable Long id, @RequestBody MyImage image) {
+
+        MyImage updatedImage = this.myImageService.update(id, image);
+
+        return ResponseEntity.ok(updatedImage);
+    }
+
 }
--- src/main/java/com/factoriaf5/myfavoriteimages/domain/services/MyImageService.java
+++ src/main/java/com/factoriaf5/myfavoriteimages/domain/services/MyImageService.java
@@ -2,6 +2,7 @@ package com.factoriaf5.myfavoriteimages.domain.services;
 
 import com.factoriaf5.myfavoriteimages.domain.models.MyImage;
 import com.factoriaf5.myfavoriteimages.infra.repositories.IMyImageRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -17,5 +18,29 @@ public class MyImageService {
 
     public List<MyImage> findAll() {
         return this.myImageRepository.findAll();
+    }
+
+    public MyImage findById(Long id) {
+        var optionalCharacter = this.myImageRepository.findById(id);
+        if(optionalCharacter.isEmpty()) throw new RuntimeException("La imagen con id: " + id + " no existe");
+        return optionalCharacter.get();
+    }
+
+    public MyImage create(MyImage image) {
+        return this.myImageRepository.save(image);
+    }
+
+    public void delete(Long id) {
+        this.myImageRepository.deleteById(id);
+    }
+
+    public MyImage update(Long id, MyImage updatedImage) {
+        MyImage existingImage = this.findById(id);
+
+        existingImage.setTitle(updatedImage.getTitle());
+        existingImage.setDescription(updatedImage.getDescription());
+        existingImage.setImage(updatedImage.getImage());
+
+        return myImageRepository.save(existingImage);
     }
 }
