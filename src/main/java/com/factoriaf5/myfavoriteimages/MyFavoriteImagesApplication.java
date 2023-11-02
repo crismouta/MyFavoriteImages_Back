3ce1e35a024533bf5bda5c1038c331bd35bb240f
crismouta <crismouta@gmail.com>
2023-11-02 20:23:33 +0100

    creating java spring boot project with postgreSQL and structure

---
+++ src/main/java/com/factoriaf5/myfavoriteimages/MyFavoriteImagesApplication.java
@@ -0,0 +1,13 @@
+package com.factoriaf5.myfavoriteimages;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFavoriteImagesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFavoriteImagesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/factoriaf5/myfavoriteimages/MyFavoriteImagesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.factoriaf5.myfavoriteimages;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFavoriteImagesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
